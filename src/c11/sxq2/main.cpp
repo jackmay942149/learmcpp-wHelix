8fbd831c31ca6baf40f4f9d3fb4f0d4743f821c5
Jack May <92079641+jackmay942149@users.noreply.github.com>
2025-02-20 12:55:10 +1000

    did ch10 and 11

---
+++ src/c11/sxq2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sal.h>
+
+// write your add function template here
+template <typename T>
+T add(T a, T b){
+  return a + b;
+}
+
+template <typename T, typename U>
+auto sub(T a, U b){
+  return a - b;
+}
+
+template <typename T>
+T multi(T a, int b){
+  return a * b;
+}
+
+int main()
+{
+	std::cout << add(2, 3) << '\n';
+	std::cout << add(1.2, 3.4) << '\n';
+	std::cout << multi(2, 3) << '\n';
+	std::cout << multi(1.2, 3) << '\n';
+  std::cout << sub(3, 2) << '\n';
+	std::cout << sub(3.5, 2) << '\n';
+	std::cout << sub(4, 1.5) << '\n';
+	return 0;
+}
